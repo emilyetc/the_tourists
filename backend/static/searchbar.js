6d32c940057a3a4e86aa860cc4e4da9041718738
Emily Wang & Atulya Lohani <alohani@dhcp-vl2041-32201.eduroam.cornell.edu>
2024-03-21 09:20:12 -0400

    added searchbar component

---
+++ backend/static/searchbar.js
@@ -0,0 +1,35 @@
+const cityInput = document.getElementById('cityInput');
+const suggestions = document.getElementById('suggestions');
+
+cityInput.addEventListener('input', async () => {
+
+  const query = cityInput.value.trim();
+  if (query === '') {
+    suggestions.innerHTML = '';
+    return;
+  }
+  const response = await fetch(`/cities?query=${query}`);
+  const cities = await response.json();
+  suggestions.innerHTML = '';
+
+  if (cities.length > 0) {
+    const suggestionsList = document.createElement('ul');
+    suggestionsList.className = "suggestions";
+
+    cities.forEach((city) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = city;
+      listItem.className = "search_result";
+      listItem.addEventListener('click', () => {
+        cityInput.value = city;
+        suggestions.innerHTML = '';
+      });
+      suggestionsList.appendChild(listItem);
+    });
+
+    suggestions.appendChild(suggestionsList);
+  }
+  else {
+
+  }
+});
