48e86b9bb2f6cd9407921e22bda2ee7a08039b61
Emily Wang & Atulya Lohani <alohani@dhcp-vl2041-27932.eduroam.cornell.edu>
2024-04-14 16:47:21 -0400

    added sql line back

--- backend/app.py
+++ backend/app.py
@@ -33,7 +33,7 @@ mysql_engine = MySQLDatabaseHandler(
 )
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
-# mysql_engine.load_file_into_db()
+mysql_engine.load_file_into_db()
 
 app = Flask(__name__)
 CORS(app)
